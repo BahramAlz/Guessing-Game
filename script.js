a4e48e0993b524e085dea620597fd067e71f2d00
bahram alzuhairi <bahram.alzuhairi@hyperisland.se>
2022-10-28 12:34:51 +0200

    Done

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
 "use strict";
 
-//Restart button
-let resetButton = document.getElementById('resetButton');
-
-let refreshPage = () => {
-    location.reload();
-}
-
-resetButton.addEventListener('click', refreshPage);
-
 //Game
 // Setting up the variables
-let btn = document.getElementById('btn');
-let output = document.getElementById('outputText');
-let attemptsTracker = document.getElementById('attemptsTracker')
+const form = document.getElementById("guessForm");
+let output = document.getElementById("outputText");
+let attemptsTracker = document.getElementById("attemptsTracker");
 
-let number = [Math.floor(Math.random() * 100)]
+let number = Math.floor(Math.random() * 10) + 1;
 
 let attempts = 0;
 
-// Adding the eventlistener & the if loop
-btn.addEventListener('click', function(){
-    let input = document.getElementById('userInput').value;
-    if (input == number){
-        output.innerHTML = (`You guessed right, your number was ${number}`);
-        output.style.color = 'green'
-        attempts--;
-    } else if (input < number){
-        output.innerHTML = "You guessed too low"
-        attemptsTracker.innerHTML = attempts += 1;
-    } else if(input > number){
-        output.innerHTML = "You guessed too high"
-        attemptsTracker.innerHTML = attempts += 1;
-    };
-    if(attempts === 10){
-        attemptsTracker.innerHTML = (`GAME OVER, your number was ${number}`);
-        attemptsTracker.style.color = ('red');
-        attempts--;
-    };
-});
+// Adding the onSubmit function & the if loop
+form.onsubmit = function (event) {
+  event.preventDefault();
+  
+  let input = Number(form.elements.input.value);
+
+  if (Number.isNaN(input)) {
+    output.innerHTML = ("That is not a number...");
+  }else if (input === number) {
+    output.innerHTML = `You guessed right, your number was ${number}`;
+    output.style.color = "green";
+    attempts--;
+  } else if (input < number) {
+    output.innerHTML = "You guessed too low";
+    attemptsTracker.innerHTML = attempts += 1;
+  } else if (input > number) {
+    output.innerHTML = "You guessed too high";
+    attemptsTracker.innerHTML = attempts += 1;
+  }
+  if (attempts === 5) {
+    attemptsTracker.innerHTML = `GAME OVER, your number was ${number}`;
+    attemptsTracker.style.color = "red";
+    attempts--;
+  }
+};
+
+//Restart button
+let resetButton = document.getElementById("resetButton");
+
+let refreshPage = () => {
+  location.reload();
+};
+
+resetButton.addEventListener("click", refreshPage);
